1d840581fa5e63140225c50ed65e5c862a61d793
nathan reginato <nathan.reginato@gmail.com>
2025-01-20 14:45:50 -0700

    feat: Remove formatter from lua LSP and add to conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,18 +1,18 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    config = function()
-	require("conform").setup({
-	  formatters_by_ft = {
-	    go = { "goimports", "gofmt" },
-	  },
-	  format_on_save = {
-	    -- These options will be passed to conform.format()
-	    timeout_ms = 500,
-	    lsp_format = "fallback",
-	  },
-	})
-    end
-  },
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					go = { "goimports", "gofmt" },
+					lua = { "stylua" },
+				},
+				format_on_save = {
+					-- These options will be passed to conform.format()
+					timeout_ms = 500,
+					lsp_format = "fallback",
+				},
+			})
+		end,
+	},
 }
-

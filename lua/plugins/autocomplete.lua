5eca59558ae222330018d088f9c4bc4d2500f2f6
nathan reginato <nathan.reginato@gmail.com>
2025-01-20 15:10:55 -0700

    feat: Added more completion sources

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -2,24 +2,46 @@ return {
 	{
 		"hrsh7th/nvim-cmp",
 		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-buffer",
-			"L3MON4D3/LuaSnip"
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"L3MON4D3/LuaSnip",
 		},
 		config = function()
-			local cmp = require 'cmp'
+			local cmp = require("cmp")
 			cmp.setup({
 				snippet = {
 					-- REQUIRED - you must specify a snippet engine
 					expand = function(args)
-						require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
 					end,
 				},
-				sources = cmp.config.sources(
-					{
-						{ name = 'buffer' },
-					}
-				)
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+				}, {
+					{ name = "buffer" },
+				}),
 			})
-		end
+
+			-- Use buffer source for `/` and `?`
+			cmp.setup.cmdline({ "/", "?" }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+
+			-- Use cmdline & path source for ':'
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{ name = "cmdline" },
+				}),
+				matching = { disallow_symbol_nonprefix_matching = false },
+			})
+		end,
 	},
 }
